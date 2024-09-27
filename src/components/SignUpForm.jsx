d5ca866e65e8c0d32d602b7b1f810fa26f24fcc5
Robert DeRieux <ppldiecomplex@gmail.com>
2024-09-27 12:55:44 -0600

    Add the html form to the SignUpForm

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,3 +1,23 @@
+import { useState } from "react";
+
 export default function SignUpForm() {
-  return <h2>Sign Up</h2>;
+  return (
+    <>
+      <h2>Sign Up</h2>
+      <form>
+        <label>
+          Username:
+          <input type="text" name="username" />
+        </label>
+        <label>
+          Password:
+          <input type="text" name="username" />
+        </label>
+        <label>
+          Error:
+          <input type="text" name="username" />
+        </label>
+      </form>
+    </>
+  );
 }
