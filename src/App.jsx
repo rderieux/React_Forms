d70d6bb9d1246ce7d63980cc07bac787ebcbfb47
Robert DeRieux <ppldiecomplex@gmail.com>
2024-09-27 12:48:47 -0600

    Add the component tags to App, remove the addButton from React boiler plate code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import "./App.css";
+import SignUpForm from "./components/SignUpForm";
+import Authenticate from "./components/Authenticate";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <SignUpForm />
+      <Authenticate />
+    </>
+  );
 }
 
 export default App;
