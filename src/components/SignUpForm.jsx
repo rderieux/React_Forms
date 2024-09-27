b65ddec0e3944d1989aad4952df30b80e05cc9ff
Robert DeRieux <ppldiecomplex@gmail.com>
2024-09-27 12:58:25 -0600

    Add variables for the username, password, and error with useState

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
 
+const [username, setUsername] = useState("");
+const [password, setPassword] = useState("");
+const [error, setError] = useState(null);
+
 export default function SignUpForm() {
   return (
     <>
