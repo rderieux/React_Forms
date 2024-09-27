cc00c5e79573e7f1cbbb125636db9aeb46862f40
Robert DeRieux <ppldiecomplex@gmail.com>
2024-09-27 13:55:08 -0600

    Pass the token to our Authenticate component, pass our setToken to our SignUpForm component

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import SignUpForm from "./components/SignUpForm";
 import Authenticate from "./components/Authenticate";
 
 function App() {
+  const [token, setToken] = useState(null);
   return (
     <>
-      <SignUpForm />
-      <Authenticate />
+      <SignUpForm setToken={setToken} />
+      <Authenticate token={token} />
     </>
   );
 }
--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,21 +1,50 @@
 import { useState } from "react";
 
-const [username, setUsername] = useState("");
-const [password, setPassword] = useState("");
-const [error, setError] = useState(null);
-
 export default function SignUpForm() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    console.log("Submitted");
+    try {
+      const response = await fetch(
+        "https://fsa-jwt-practice.herokuapp.com/signup",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application.json" },
+          body: JSON.stringify(),
+        }
+      );
+      const result = await response.json();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <>
       <h2>Sign Up</h2>
-      <form>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
         <label>
           Username:
-          <input type="text" name="username" />
+          <input
+            type="text"
+            name="username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
         </label>
         <label>
           Password:
-          <input type="text" name="username" />
+          <input
+            type="text"
+            name="username"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
         </label>
         <button>Submit</button>
       </form>
